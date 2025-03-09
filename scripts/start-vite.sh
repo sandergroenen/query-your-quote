5a398a6ded25549e356f20ef876f666a7e9f25cc
sander <sangroenen@gmail.com>
2025-03-09 19:21:13 +0100

    005: fix circular reference, delete obsolete script

--- scripts/start-vite.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Script to start the Vite development server in the app container
-echo "Starting Vite development server in the background..."
-docker compose exec -d app bash -c "cd /var/www && npm run vite &"
-echo "Vite development server is running at http://localhost:5173"
